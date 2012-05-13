3b352121927823f4b5fc85ff96180710bca1ecc2
Luca Matteis <lmatteis@gmail.com>
2012-05-13 22:23:19 +0200

    much better ordering of items

--- app.js
+++ app.js
@@ -1,53 +1,61 @@
-var couchapp = require('couchapp')
-  , path = require('path')
-  ;
+var couchapp = require('couchapp'), 
+    path = require('path');
 
-ddoc = 
-  { _id:'_design/news'
-  , rewrites : 
-    [ {from:"/", to:'_list/items/items'}
-    , {from:"/login", to:'login.html'}
-    , {from:"/api", to:'../../'}
-    , {from:"/api/*", to:'../../*'}
-    , {from:"/*", to:'*'}
+ddoc = { 
+    _id:'_design/news',
+    rewrites : [
+        {from:'/', to:'_list/items/items'},
+        {from:'/login', to:'login.html'},
+        {from:'/api', to:'../../'},
+        {from:'/api/*', to:'../../*'},
+        {from:'/*', to:'*'}
     ]
-  }
-  ;
-
-ddoc.views = {
-  items: {
-    map: function(doc) {
-      if(doc.type && doc.type === "item") {
-        emit(doc.title, doc.points);
-      }
-    }
-  }
 };
 
-ddoc.lists = {
-  items: function(head, req) {
-    provides("html", function(){
-      var row,
-          Mustache = require("views/lib/mustache"),
-          data = {
-            title: "All Items",
+ddoc.views = {};
+
+ddoc.views.items = {
+    map: function(doc) {
+        if(doc.path && doc.points) {
+            // doc.path should always be an array
+            var keys = [];
+            for(var i=0; i<doc.path.length; i++) {
+                keys.push(doc.path[i]);
+            }
+            // add its own id as shown here http://probablyprogramming.com/2008/07/04/storing-hierarchical-data-in-couchdb
+            keys.push(doc._id);
+
+            keys.push(doc.points);
+
+            emit(keys, doc.points);
+        }
+    }
+};
+
+ddoc.lists = {};
+ddoc.lists.items = function(head, req) {
+    provides('html', function(){
+        var row,
+            Mustache = require('views/lib/mustache');
+
+        var data = {
+            title: 'All Items',
             username: req.userCtx.name,
             login: !(req.userCtx.name),
             rows: []
-          };
+        };
 
-      while(row = getRow()) {
-        data.rows.push(row);
-      }
-      var html = Mustache.to_html(this.templates.items, data, this.templates.partials);
-      return html;
+        while(row = getRow()) {
+            data.rows.push(row);
+        }
+        var html = Mustache.to_html(this.templates.items, data, this.templates.partials);
+        return html;
     });
-  }
 }
 
 ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
   if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
-    throw "Only admin can delete documents on this database.";
+    throw 'Only admin can delete documents on this database.';
   } 
 }
 
