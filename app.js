4ce4d534a382950b091d2e9d7f2390b0f8a5fc84
Luca Matteis <lmatteis@gmail.com>
2012-05-16 17:21:20 +0200

    achieved some sort of hierarchy

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var couchapp = require('couchapp'),
 ddoc = { 
     _id:'_design/news',
     rewrites : [
-        {from:'/', to:'_list/items/items'},
+        {from:'/', to:'_list/all/items'},
+        {from:'/item', to:'_list/item/items', query: { startkey: [":id"], endkey: [":id",{}] } },
         {from:'/login', to:'login.html'},
         {from:'/api', to:'../../'},
         {from:'/api/*', to:'../../*'},
@@ -27,15 +28,43 @@ ddoc.views.items = {
 
             //keys.push(doc.points * -1);
 
-            emit(keys, 1);
-        } else if(doc.type === 'vote') {
-            emit(doc.item_id, 1);
+            emit(keys, doc);
         }
     }
+    /*
+    ,
+    reduce: function(keys, values, rereduce) {
+        var tree = {};
+        var current;
+        if(!rereduce) {
+            for (var i in keys) {
+                current = tree;
+                for (var j in keys[i][0]) {
+                    child = keys[i][0][j];
+                    if (current[child] == undefined) 
+                        current[child] = {};
+                    current = current[child];
+                } 
+                current['_data'] = keys[i][0];
+            }
+        } else if(rereduce) {
+            for(var i in values) {
+                current = values[i];
+                for (var j in keys[i][0]) {
+                    child = keys[i][0][j];
+                    if (current[child] == undefined) 
+                        current[child] = {};
+                    current = current[child];
+                } 
+            }
+        }
+        return tree;
+    }
+    */
 };
 
 ddoc.lists = {};
-ddoc.lists.items = function(head, req) {
+ddoc.lists.all = function(head, req) {
     provides('html', function(){
         var row,
             Mustache = require('views/lib/mustache');
@@ -50,7 +79,29 @@ ddoc.lists.items = function(head, req) {
         while(row = getRow()) {
             data.rows.push(row);
         }
-        var html = Mustache.to_html(this.templates.items, data, this.templates.partials);
+        var html = Mustache.to_html(this.templates.all, data, this.templates.partials);
+        return html;
+    });
+};
+
+ddoc.lists.item = function(head, req) {
+    provides('html', function(){
+        var row,
+            Mustache = require('views/lib/mustache');
+
+        var data = {
+            title: 'Item',
+            username: req.userCtx.name,
+            login: !(req.userCtx.name),
+            item: getRow()["value"], 
+            rows: []
+        };
+
+        while(row = getRow()) {
+            row.indent = (row.key.length - 2) * 40;
+            data.rows.push(row);
+        }
+        var html = Mustache.to_html(this.templates.item, data, this.templates.partials);
         return html;
     });
 }
