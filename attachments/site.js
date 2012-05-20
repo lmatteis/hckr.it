6c6b2b74fcd1ebdeef8cd7b7fe48b7c4f982e83d
Luca Matteis <lmatteis@gmail.com>
2012-05-20 13:25:05 +0200

    wow ORDER comments

--- attachments/site.js
+++ attachments/site.js
@@ -1,60 +1,31 @@
-var request = function (options, callback) {
-  options.success = function (obj) {
-    callback(null, obj);
-  }
-  options.error = function (err) {
-    if (err) callback(err);
-    else callback(true);
-  }
-  if (options.data && typeof options.data == 'object') {
-    options.data = JSON.stringify(options.data)
-  }
-  if (!options.dataType) options.processData = false;
-  if (!options.dataType) options.contentType = 'application/json';
-  if (!options.dataType) options.dataType = 'json';
-  $.ajax(options)
+function getCommentsByParentId(level, parentId) {
+    var $comments = $('.comments [parent_id=' + parentId + ']');
+
+    // sort these comments by points
+    $comments.sort(function(a, b) {
+        var k1 = parseInt($(a).attr('points'), 10); 
+        var k2 = parseInt($(b).attr('points'), 10);
+        return (k1 > k2) ? 1 : ( (k2 > k1) ? -1 : 0 );
+    });
+
+    $comments.each(function() {
+        // let's clone it so it doesn't effect the actual HTML
+        var $this = $(this).clone();
+        $this.show();
+        // show them by appending at this level
+        if(!level) {
+            $('.comments').append($this);
+        } else {
+            // get the level width property
+            // and add 40 to it
+            var width = level.find('.level').attr('width');
+            $this.find('.level').attr('width', parseInt(width, 10) + 40);
+            $this.insertAfter(level);
+        }
+        getCommentsByParentId($this, $this.attr('doc_id'));
+    });
+
 }
-
-$.expr[":"].exactly = function(obj, index, meta, stack){ 
-  return ($(obj).text() == meta[3])
-}
-
-var param = function( a ) {
-  // Query param builder from jQuery, had to copy out to remove conversion of spaces to +
-  // This is important when converting datastructures to querystrings to send to CouchDB.
-	var s = [];
-	if ( jQuery.isArray(a) || a.jquery ) {
-		jQuery.each( a, function() { add( this.name, this.value ); });		
-	} else { 
-	  for ( var prefix in a ) { buildParams( prefix, a[prefix] ); }
-	}
-  return s.join("&");
-	function buildParams( prefix, obj ) {
-		if ( jQuery.isArray(obj) ) {
-			jQuery.each( obj, function( i, v ) {
-				if (  /\[\]$/.test( prefix ) ) { add( prefix, v );
-				} else { buildParams( prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") +"]", v )}
-			});				
-		} else if (  obj != null && typeof obj === "object" ) {
-			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });				
-		} else { add( prefix, obj ); }
-	}
-	function add( key, value ) {
-		value = jQuery.isFunction(value) ? value() : value;
-		s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
-	}
-}
-
-var app = {};
-app.index = function () {
-  
-};
-
-$(function () { 
-  app.s = $.sammy(function () {
-    // Index of all databases
-    this.get('', app.index);
-    this.get("#/", app.index);
-  })
-  app.s.run();
+$(function() {
+    getCommentsByParentId(0, '7f801b5e1ac680fbc112fa014a0003d0');
 });
