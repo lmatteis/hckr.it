211e3beaf217f4f3746d50c02fa10b5ffa17a589
Luca Matteis <lmatteis@gmail.com>
2012-05-26 15:40:03 +0200

    fixed some issues with config

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     conf_title: 'hckr.it',
     conf_path : '/',
+    conf_logo : 'http://i.imgur.com/jgLZr.gif',
+    conf_about : 'about',
     conf_morelink : 'More',
     conf_addcomment : 'add comment',
     conf_pointsby : 'points by',
@@ -8,7 +10,7 @@ module.exports = {
     conf_username : 'username',
     conf_password : 'password',
     conf_login : 'login',
-    conf_newestlink : 'newest',
+    conf_logout : 'logout',
     conf_newest : 'new',
     conf_submit : 'submit',
     conf_submittitle : 'title'
