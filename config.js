98f7f5653980305ac008edece657bc3a00694e7a
Luca Matteis <lmatteis@gmail.com>
2012-05-26 15:23:38 +0200

    can translate by simply editing config.js

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 module.exports = {
     conf_title: 'hckr.it',
-    conf_path : '/'
+    conf_path : '/',
+    conf_morelink : 'More',
+    conf_addcomment : 'add comment',
+    conf_pointsby : 'points by',
+    conf_commentreply : 'reply',
+    conf_username : 'username',
+    conf_password : 'password',
+    conf_login : 'login',
+    conf_newestlink : 'newest',
+    conf_newest : 'new',
+    conf_submit : 'submit',
+    conf_submittitle : 'title'
 };
