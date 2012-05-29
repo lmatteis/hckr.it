3eeaccee5dee172b01a6b053c6fdfeae30f36a63
Luca Matteis <lmatteis@gmail.com>
2012-05-30 00:04:12 +0200

    added color config property

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     conf_perpage : '30',
 
     conf_logo : 'http://i.imgur.com/jgLZr.gif',
+    conf_color: '#D9E7FF',
     conf_about : 'about',
     conf_morelink : 'More',
     conf_addcomment : 'add comment',
